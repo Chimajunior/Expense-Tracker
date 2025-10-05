446770e867383d7c80af91f7789e21dcead220d5
chima <chimajunior14@gmail.com>
2025-10-05 02:37:44 +0100

    added cookie options

--- server/src/controllers/authControllers.js
+++ server/src/controllers/authControllers.js
@@ -1,3 +1,11 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwentoken');
 const User = require('../models/User');
+
+const cookieOptions = {
+    hhtpOnly: true,
+    sameSite: 'lax',
+    secure: false, // set to true for prod
+    path: '/',
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+};
