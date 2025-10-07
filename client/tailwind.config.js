4ea373a74dded46b251edd10a66755a0c442a97f
chima <chimajunior14@gmail.com>
2025-10-08 00:53:53 +0100

    initial comm

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,31 +1,52 @@
-// /** @type {import('tailwindcss').Config} */
-// export default {
-//     content: ['./index.html', './src/**/*.{js,jsx}'],
-//     theme: {
-//       extend: {
-//         colors: {
-//           brand: { red: '#D71920', ink: '#0B2239' },
-//         },
-//       },
-//     },
-//     plugins: [],
-//   }
-
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  content: ['./index.html', './src/**/*.{js,jsx}'],
   theme: {
     extend: {
       colors: {
         brand: {
-          red: "#D71920", // your main accent – action buttons, highlights
-          redDark: "#BE161C", // hover version for buttons
-          ink: "#0B2239", // dark navy for text & navbar
-          gray: "#F8FAFC", // light background panels
-          success: "#10B981", // green for income / positive amounts
+          red: '#D71920',
+          redDark: '#BE161C',
+          ink: '#0B2239',
+          gray: '#F8FAFC',
+          success: '#10B981',
         },
       },
     },
   },
   plugins: [],
-};
+}
+
+
+
+// // /** @type {import('tailwindcss').Config} */
+// // export default {
+// //     content: ['./index.html', './src/**/*.{js,jsx}'],
+// //     theme: {
+// //       extend: {
+// //         colors: {
+// //           brand: { red: '#D71920', ink: '#0B2239' },
+// //         },
+// //       },
+// //     },
+// //     plugins: [],
+// //   }
+
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: ["./index.html", "./src/**/*.{js,jsx}"],
+//   theme: {
+//     extend: {
+//       colors: {
+//         brand: {
+//           red: "#D71920", // your main accent – action buttons, highlights
+//           redDark: "#BE161C", // hover version for buttons
+//           ink: "#0B2239", // dark navy for text & navbar
+//           gray: "#F8FAFC", // light background panels
+//           success: "#10B981", // green for income / positive amounts
+//         },
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
