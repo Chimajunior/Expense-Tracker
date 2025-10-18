3a20427230bf9d5614d4b0f0ec84ffa6cd07cab4
chima <chimajunior14@gmail.com>
2025-10-18 01:14:57 +0100

    initial com for currencies

---
+++ server/src/constants/Currencies.js
@@ -0,0 +1,9 @@
+
+//Currencies
+module.exports = {
+    SUPPORTED_CURRENCIES: ['GBP','USD','EUR','NGN','KES','ZAR'],
+    MINOR_UNITS: { // number of decimal places
+      GBP: 2, USD: 2, EUR: 2, NGN: 2, KES: 2, ZAR: 2
+    }
+  };
+  
