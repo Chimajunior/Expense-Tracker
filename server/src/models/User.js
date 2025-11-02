4bf033d7213b55a8473dc946ca5b1a8010a7bbe7
chima <chimajunior14@gmail.com>
2025-11-02 23:23:05 +0000

    new user.js commit

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,12 +1,14 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+const { SUPPORTED_CURRENCIES } = require('../constants/currencies');
 
 const UserSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     email: { type: String, unique: true, required: true, index: true },
     passwordHash: { type: String, required: true },
+    defaultCurrency: { type: String, enum: SUPPORTED_CURRENCIES, default: 'GBP' },
   },
   { timestamps: true }
 );
 
-module.exports = mongoose.model("User", UserSchema);
+module.exports = mongoose.model('User', UserSchema);
